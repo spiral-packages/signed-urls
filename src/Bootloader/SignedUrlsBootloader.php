4bc598b8eab01f836706cee5f0e70276348cd492
butschster <butschster@gmail.com>
2022-06-16 23:33:40 +0400

    Init project

---
+++ src/Bootloader/SignedUrlsBootloader.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\SignedUrls\Bootloader;
+
+use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Core\Container;
+
+class SignedUrlsBootloader extends Bootloader
+{
+
+}
