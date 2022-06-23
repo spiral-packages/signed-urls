c378fe445e9e23e3410fd5ca8493a3cbecfe1f6c
butschster <butschster@gmail.com>
2022-06-23 19:34:55 +0400

    Fixes base exception class for InvalidSignatureException

--- src/Exception/InvalidSignatureException.php
+++ src/Exception/InvalidSignatureException.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Spiral\SignedUrls\Exception;
 
-class InvalidSignatureException extends SignedUrlsException
-{
+use Spiral\Http\Exception\ClientException;
 
+class InvalidSignatureException extends ClientException
+{
+    public function __construct(string $message = 'Invalid signature.')
+    {
+        parent::__construct(403, $message);
+    }
 }
